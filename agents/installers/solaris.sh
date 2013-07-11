86313c046e32d888a55b6e231022c18e56602d98
Super-User <root@mci1-infcomp03.int.ramtank.com>
2013-07-11 22:05:32 +0000

    Adds make dir

--- agents/installers/solaris.sh
+++ agents/installers/solaris.sh
@@ -41,7 +41,7 @@ case $zonename in
 				UNAME=omnios
 				EXECUTABLES="mpathadm"
 				PKGINSTALL="pkg install"
-				DESTPATH="/usr/bin/check_mk_agent"
+				DESTPATH="/usr/bin"
 				SOURCEMANIFEST=${basedir}/support_solaris/check_mk-tcp.xml.omnios
 				DESTMANIFEST=/lib/svc/manifest/network/check_mk-tcp.xml
 				DESTLIBDIR=/opt/local/lib/check_mk_agent
@@ -51,7 +51,7 @@ case $zonename in
 				UNAME=smartos
 				EXECUTABLES="mpathadm"
 				PKGINSTALL="pkgin in"
-				DESTPATH="/opt/custom/bin/check_mk_agent"
+				DESTPATH="/opt/custom/bin"
 				SOURCEMANIFEST=${basedir}/support_solaris/check_mk-tcp.xml.smartos
 				DESTMANIFEST=/opt/custom/smf/check_mk-tcp.xml
 				DESTLIBDIR=/opt/custom/lib/check_mk_agent
@@ -70,7 +70,7 @@ case $zonename in
 				UNAME=smartos
 				EXECUTABLES="mpathadm"
 				PKGINSTALL="pkgin in"
-				DESTPATH="/opt/local/bin/check_mk_agent"
+				DESTPATH="/opt/local/bin"
 				SOURCEMANIFEST=${basedir}/support_solaris/check_mk-tcp.xml.smartoszone
 				DESTMANIFEST=/var/svc/manifest/network/check_mk-tcp.xml
 				DESTLIBDIR=/opt/local/lib/check_mk_agent
@@ -118,13 +118,14 @@ else
 	echo "check_mk	6556/tcp" >> /etc/services
 fi
 
-if [ -f ${DESTPATH} ] && [ $FORCE -ne 1 ] 
+if [ -f ${DESTPATH}/check_mk_agent ] && [ $FORCE -ne 1 ] 
 then
 	echo "Found check_mk agent."
 else
 	echo "Installing check_mk agent."
-  cat ${agentpath} | sed -e "s:export MK_LIBDIR=.*$:export MK_LIBDIR=${DESTLIBDIR}:" | sed -e "s:export MK_CONFDIR=.*$:export MK_CONFDIR=${DESTCONFDIR}:" > ${DESTPATH}
-  chmod +x ${DESTPATH}
+	mkdir -p ${DESTPATH}
+	cat ${agentpath} | sed -e "s:export MK_LIBDIR=.*$:export MK_LIBDIR=${DESTLIBDIR}:" | sed -e "s:export MK_CONFDIR=.*$:export MK_CONFDIR=${DESTCONFDIR}:" > ${DESTPATH}
+  	chmod +x ${DESTPATH}
 fi
 
 if svcs check_mk/tcp:default 1> /dev/null 
